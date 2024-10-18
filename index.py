b2878a0526602383eec808f2e1091d0cbdc30a7f
Hanltn <136073168+Hanltn@users.noreply.github.com>
2024-10-18 14:40:43 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import dao
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    categories = dao.load_categories()
+    return render_template('index.html', cate=categories)
+
+if __name__ == "__main__":
+    app.run(debug=True)
